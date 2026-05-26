b74dee59a850ca13198b702828ec71023d1a633a
SakurabaKawaii <sanstale6@outlook.com>
2026-05-26 23:02:32 +0800

    Last Sync: 2026-05-26 23:02 (Mobile)

--- scripts/fruit_component.gd
+++ scripts/fruit_component.gd
@@ -5,28 +5,37 @@ class_name FruitComponent
 @export var collision : CollisionShape2D
 @export var fruit_id : String
 @export var detect_area : Area2D
-@export var target : PackedScene
+@export_file var target_fruit
 @export var anim : AnimationPlayer
+var making : bool = false
 
 func _ready() -> void:
 	detect_area.body_entered.connect(collision_judge)
+	target_fruit = load(target_fruit)
 	
 
 
 func collision_judge(body):
-	print(6)
+	if !body.is_in_group('fruit'):
+		return
 	var target_fruit_component = body.get_node('FruitComponent')
-	if target_fruit_component.fruit_id == fruit_id:
+	if target_fruit_component.making :
+		return
+	if target_fruit_component.fruit_id == fruit_id and target_fruit_component.get_instance_id() != get_instance_id():
 		integrate(target_fruit_component)
 		
 func integrate(companion : FruitComponent) -> void:
+	if making:
+		return
+	making = true
 	collision.disabled = true
 	detect_area.get_node('CollisionShape2D').disabled = true
-	#fruit.get_node('Sprite2D').invisible = false
-	if get_instance_id() > companion.get_instance_id():
+	fruit.get_node('Sprite2D').visible = false
+	if get_instance_id() < companion.get_instance_id():
 		return
 	anim.play('break')
-	var spawn_position = (position + companion.position)/2
-	#var spawned_fruit = target.instantiate()
-	#spawned_fruit.position = spawn_position
-	#get_tree().current_scene.add_child(spawned_fruit)
+	var spawn_position = (get_parent().position + companion.get_parent().position)*0.5
+	print(spawn_position)
+	var spawned_fruit = target_fruit.instantiate()
+	spawned_fruit.position = spawn_position
+	get_tree().current_scene.add_child(spawned_fruit)
